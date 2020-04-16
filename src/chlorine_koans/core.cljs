bdc442534b94df9082cd6fa65d12c2efbde02c55
Maurício Szabo <mauricio.szabo@gmail.com>
2020-04-16 14:30:32 -0300

    Core fix

---
+++ src/chlorine_koans/core.cljs
@@ -0,0 +1,31 @@
+(ns chlorine-koans.core
+  (:require [chlorine-koans.aux :refer [subscriptions] :as aux]
+            [chlorine-koans.koans.cljs :as cljs]
+            [chlorine-koans.ui.inline :as inline]
+            [chlorine-koans.ui.atom :as atom]
+            ["atom" :refer [CompositeDisposable]]))
+
+(defonce atom-state (atom nil))
+
+(defn- info! [message]
+  (.. js/atom -notifications (addInfo message)))
+
+(defn activate [state]
+  (reset! aux/plugin-dir js/__dirname)
+  (reset! atom-state state)
+  (cljs/connect!)
+  (.add @aux/subscriptions (.. js/atom -commands
+                               (add "atom-text-editor"
+                                    "chlorine-koans:clear-inline-results"
+                                    #(inline/clear-results! (atom/current-editor))))))
+
+(defn deactivate [state]
+  (.dispose ^js @subscriptions))
+
+(defn ^:dev/before-load reset-subs []
+  (deactivate @atom-state))
+
+(defn ^:dev/after-load re-activate []
+  (reset! subscriptions (CompositeDisposable.))
+  (activate @atom-state)
+  (info! "Reloaded plug-in"))
