f5025ac1541336b7297fb2749f33d49eaec8b743
Maurício Szabo <mauricio.szabo@gmail.com>
2020-04-19 15:13:36 -0300

    Organizing meditations

--- src/chlorine_koans/koans/cljs_koans.cljs
+++ src/chlorine_koans/koans/cljs_koans.cljs
@@ -9,10 +9,11 @@
             [chlorine-koans.koans.cljs :as cljs]))
 
 (defonce state (atom {:current-meditation 0
-                      :current-koan 0}))
+                      :current-koan 0
+                      :meditations []}))
 
 #_
-(reset! state {:current-meditation 8 :current-koan 0})
+(swap! state merge {:current-meditation 6 :current-koan 0})
 
 (defn- make-unification [code]
  (walk/prewalk-replace {:__ '?code} code))
@@ -31,9 +32,6 @@
         meditation-code (u/unify '(goto-exercise ?ex) code)]
     (prn :KOAN koan-code)
     (prn :EX meditation-code)))
-  ; (case code
-  ;   '(goto-koan 1)
-  ;   '(goto-koan 1)))
 
 (defn- enlightenmnent! []
   (cljs/handle-result! gotos)
@@ -41,12 +39,12 @@
 
 (declare meditate-over! start!)
 (defn- new-koan! []
-  (if (-> meditations/categories count dec (< (:current-koan @state)))
+  (if (-> meditations/categories count dec (> (:current-koan @state)))
     (swap! state update :current-koan inc)
     (swap! state assoc :current-koan :end))
   (start!))
 
-(defn- score-meditation [meditations {:keys [assert]} {:keys [res code]}]
+(defn- score-meditation [{:keys [assert]} {:keys [res code]}]
   (let [u (and (= res true) (u/unify code assert))
         code (get u '?code)]
     (when (and code (not= code :__))
@@ -54,25 +52,25 @@
                           (get u '?code) "\""))
       (p/do!
        (p/delay 300)
-       (if (-> meditations count dec (< (:current-meditation @state)))
+       (if (-> @state :meditations count dec (> (:current-meditation @state)))
          (do
            (swap! state update :current-meditation inc)
-           (meditate-over! meditations))
+           (meditate-over!))
          (new-koan!))))))
 
-(defn- meditate-over! [meditations]
-  (let [meditation (nth meditations (:current-meditation @state))]
+(defn- meditate-over! []
+  (let [meditation (nth (:meditations @state) (:current-meditation @state))]
     (atom/append-on-editor {:contents (str "\n\n" (:message meditation))})
-    (cljs/handle-result! #(score-meditation meditations meditation %))))
+    (cljs/handle-result! #(score-meditation meditation %))))
 
 (defn start! []
   (if (-> @state :current-koan (= :end))
     (enlightenmnent!)
     (let [curr-koan (nth meditations/categories (:current-koan @state))
           name (:name curr-koan)
-          meditations (prepare-meditations curr-koan)
           file-name (str "src/koans/" name ".cljs")]
+      (swap! state assoc :meditations (prepare-meditations curr-koan))
       (p/let [editor (atom/open-editor {:file-name file-name})]
         (when (-> ^js editor .getText str/trim empty?)
           (atom/append-on-editor {:contents (str ";; " (str/capitalize name))}))
-        (meditate-over! meditations)))))
+        (meditate-over!)))))
